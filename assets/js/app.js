d68045eab4ddb66c1943ffefb3a373b0c2d0f50f
ilyaasAS <ilyaas.95.jv@gmail.com>
2024-07-23 11:17:57 +0200

    update

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+// Mettre cette function retadatisse ne charge que quand le DOM est charger; et mettre tous son js dans cette function retardisse //
+document.addEventListener('DOMContentLoaded', function() {
+    alert('Bonjour')
+
+})
