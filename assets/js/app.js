63ecc21bd51de2ab200fceaa1103dfd0ae100cab
ilyaasAS <ilyaas.95.jv@gmail.com>
2024-07-24 11:38:39 +0200

    Squellete validé

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 // Mettre cette function retadatisse ne charge que quand le DOM est charger; et mettre tous son js dans cette function retardisse //
 document.addEventListener('DOMContentLoaded', function() {
-    alert('Bonjour')
+    // Fonction pour faire défiler vers les spécialités
+    window.scrollToSpecials = function() {
+        document.getElementById('specials').scrollIntoView({ behavior: 'smooth' });
+    }
 
 }) 
+
