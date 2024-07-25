c26f972400fb3105148da7f4284ae5ab96af1f59
ilyaasAS <ilyaas.95.jv@gmail.com>
2024-07-25 09:09:27 +0200

    update V2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,17 @@
-// Mettre cette function retadatisse ne charge que quand le DOM est charger; et mettre tous son js dans cette function retardisse //
 document.addEventListener('DOMContentLoaded', function() {
     // Fonction pour faire défiler vers les spécialités
     window.scrollToSpecials = function() {
         document.getElementById('specials').scrollIntoView({ behavior: 'smooth' });
     }
 
-}) 
+    // Exemple d'autres initialisations ou fonctions que vous pourriez avoir besoin d'exécuter
+    // lorsque le DOM est complètement chargé.
+    // Par exemple, initialisation de la classe active pour les liens de navigation.
+    const links = document.querySelectorAll('.nav-right a');
+    links.forEach(link => {
+        if (link.href === window.location.href) {
+            link.classList.add('active');
+        }
+    });
+});
 
